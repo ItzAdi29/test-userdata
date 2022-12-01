55179732df02d37df5eac949dd72020ba698a1e4
ItzAdi29 <91110158+ItzAdi29@users.noreply.github.com>
2022-12-01 16:28:31 +0000

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "private-sg" {
+  name = "my-SG"
+  description = "HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my-EC2" {
+  ami = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+  key_name = "test-key"
+  vpc_security_group_ids = [aws_security_group.private-sg.id]
+  associate_public_ip_address = true
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+  }
+  user_data = <<EOF
+#!/bin/bash
+cd /opt
+wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
+tar -xvzf /opt/apache-tomcat-9.0.69.tar.gz
+mv apache-tomcat-9.0.69.tar.gz tomcat9
+chmod +x /opt/tomcat9/bin/startup.sh 
+chmod +x /opt/tomcat9/bin/shutdown.sh
+ln -s /opt/tomcat9/bin/startup.sh /usr/local/bin/tomcatup
+ln -s /opt/tomcat9/bin/shutdown.sh /usr/local/bin/tomcatdown
+tomcatup
+
+EOF
+
+  tags = {
+    Name = "Test-EC2"
+  }
+}
